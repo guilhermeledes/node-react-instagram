5050bb6e04080106e3cf509c67fde650b916b23f
Guilherme Ledes <ledes.guilherme@gmail.com>
2020-03-29 21:54:26 -0300

    criação do repositório

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,42 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+
+const Post = require('../models/Post');
+
+module.exports = {
+    async index(req, res) {
+        const post = await Post.find().sort('-createdAt');
+
+        return res.json(post);
+    },
+
+    async store(req, res) {
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+
+        const [ name, ext ] = image.split('.');
+        const filename = `${name}.jpg`;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            );
+        
+        fs.unlinkSync(req.file.path);
+        
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image: filename
+        });
+
+        req.io.emit('post', post);
+
+        return res.json(post);
+    }
+}
